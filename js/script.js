3dc5e2bb8e5945e677ae87c8a3575fb161ed091b
Wesley Mattos <wesleysniper1.6@gmail.com>
2026-04-27 21:22:51 -0300

    pagina de serviços adicionados com foto e descrição

--- js/script.js
+++ js/script.js
@@ -82,3 +82,16 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
+
+
+
+const items = document.querySelectorAll(".accordion-item");
+
+items.forEach(item => {
+  const header = item.querySelector(".accordion-header");
+
+  header.addEventListener("click", () => {
+    item.classList.toggle("active");
+  });
+});
+
